51bef9ae3171e2423d757e62cd16880dc3706b28
will-twd <hello@willthewebdev.com>
2026-08-26 15:54:31 +0100

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import laravel from 'laravel-vite-plugin'
+import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
+
+// Set APP_URL if it doesn't exist for Laravel Vite plugin
+if (! process.env.APP_URL) {
+  process.env.APP_URL = 'http://example.test';
+}
+
+export default defineConfig({
+  base: '/wp-content/themes/positivity-mobility/public/build/',
+  plugins: [
+    tailwindcss(),
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+        'resources/css/editor.css',
+        'resources/js/editor.js',
+      ],
+      refresh: true,
+      assets: ['resources/images/**', 'resources/fonts/**'],
+    }),
+
+    wordpressPlugin(),
+
+    // Generate the theme.json file in the public/build/assets directory
+    // based on the Tailwind config and the theme.json file from base theme folder
+    wordpressThemeJson({
+      disableTailwindColors: false,
+      disableTailwindFonts: false,
+      disableTailwindFontSizes: false,
+      disableTailwindBorderRadius: false,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@scripts': '/resources/js',
+      '@styles': '/resources/css',
+      '@fonts': '/resources/fonts',
+      '@images': '/resources/images',
+    },
+  },
+})
