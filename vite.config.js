8354ad2c44e204070177de4f574e922cb721d01d
will-twd <hello@willthewebdev.com>
2026-08-26 16:32:32 +0100

    configure vite to work with DDEV, test hmr working

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import laravel from 'laravel-vite-plugin'
+import laravel from 'laravel-vite-plugin';
 import { wordpressPlugin, wordpressThemeJson } from '@roots/vite-plugin';
 
 // Set APP_URL if it doesn't exist for Laravel Vite plugin
-if (! process.env.APP_URL) {
+if (!process.env.APP_URL) {
   process.env.APP_URL = 'http://example.test';
 }
 
 export default defineConfig({
   base: '/wp-content/themes/positivity-mobility/public/build/',
+
+  // Adjust Vite's dev server to work with DDEV
+  // https://vitejs.dev/config/server-options.html
+  server: {
+    // Respond to all network requests
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    // Defines the origin of the generated asset URLs during development,
+    // this must be set to the Vite dev server URL and selected port.
+    origin: `${process.env.DDEV_PRIMARY_URL_WITHOUT_PORT}:5173`,
+    // Configure CORS securely for the Vite dev server to allow requests
+    // from *.ddev.site domains, supports additional hostnames (via regex).
+    // If you use another `project_tld`, adjust this value accordingly.
+    cors: {
+      origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+    },
+  },
+
   plugins: [
     tailwindcss(),
     laravel({
@@ -42,4 +61,4 @@ export default defineConfig({
       '@images': '/resources/images',
     },
   },
-})
+});
